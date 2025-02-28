62d9dfd76680e713093c97dd6bd385f420d776c1
Gabriela de Souza Leopoldino <144043226+GabiihLeopoldino@users.noreply.github.com>
2025-02-28 10:48:10 -0300

    Aula4

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const aumentaFonteBotão = document.getElementById ('aumentar-fonte') ;
+
+    let tamanhoAtualFonte = 1;
+
+    aumentaFonteBotão.addEventListener('click', function(){
+        tamanhoAtualFonte + = 0.1;    
+    })
+
+
+})
